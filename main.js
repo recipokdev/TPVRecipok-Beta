e8f66194ce14bd99ad37c64640a25f93028601da
recipokdev <recipokdev@gmail.com>
2026-01-19 09:34:07 +0100

    chore(release): v0.1.35-beta.2

--- main.js
+++ main.js
@@ -260,7 +260,7 @@ function createSplashWindow() {
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TPV Recipok (BETA)</title>
+    <title>TPV Recipok</title>
     <style>
       body{
         margin:0;
